0e71f671647d20af93b24962ecc8d6b2eb0f2b64
Daniel Nikolov <dnikolov@gmail.com>
2020-05-07 12:24:58 +0300

    Initial Project skeleton

---
+++ src/test/java/org/hcl/app/OrderapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hcl.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
