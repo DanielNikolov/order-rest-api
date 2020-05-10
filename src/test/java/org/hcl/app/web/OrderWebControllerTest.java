ce888d7164eaecfe1720296899ba588b95a68610
Daniel Nikolov <dnikolov@gmail.com>
2020-05-10 07:38:35 +0300

    Updated test cases

--- src/test/java/org/hcl/app/web/OrderWebControllerTest.java
+++ src/test/java/org/hcl/app/web/OrderWebControllerTest.java
@@ -1,14 +1,19 @@
 package org.hcl.app.web;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.List;
+
 import org.hcl.app.business.domain.APIResponse;
 import org.hcl.app.business.domain.Sales;
 import org.hcl.app.business.service.OrderService;
@@ -61,7 +66,7 @@ public class OrderWebControllerTest {
 	@Test
 	public void testCreateOrder() throws JsonProcessingException, Exception {
 		APIResponse apiResponse = new APIResponse();
-		apiResponse.setResponse(1);
+		apiResponse.setResponse(1L);
 		BDDMockito.given(orderService.createOrder(sales)).willReturn(apiResponse);
 		mockMvc.perform(
 				post("/api/orders")
@@ -71,5 +76,44 @@ public class OrderWebControllerTest {
 				.accept(MediaType.APPLICATION_JSON)
 			)
 			.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void testDeleteOrder() throws Exception {
+		APIResponse apiResponse = new APIResponse();
+		sales.setSalesId(1L);
+		apiResponse.setResponse(1L);
+		BDDMockito.given(orderService.deleteOrder(1L)).willReturn(apiResponse);
+		mockMvc.perform(delete("/api/orders?orderid=1"))
+			.andExpect(status().isOk())
+			.andExpect(content().string(containsString("\"response\":1")));
+	}
+	
+	@Test
+	public void testUpdateOrder() throws Exception {
+		APIResponse apiResponse = new APIResponse();
+		sales.setSalesId(1L);
+		apiResponse.setResponse(1L);
+		BDDMockito.given(orderService.updateOrder(sales)).willReturn(apiResponse);
+		mockMvc.perform(
+				put("/api/orders")
+				.content(new ObjectMapper().writeValueAsString(sales))
+				.characterEncoding("UTF-8")
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)
+			)
+			.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void testGetSalesByDate() throws Exception {
+		APIResponse apiResponse = new APIResponse();
+		sales.setSalesId(1L);
+		List<Sales> salesList = Arrays.asList(new Sales[] { sales });
+		apiResponse.setResponse(salesList);
+		BDDMockito.given(orderService.getOrdersByDate("2020-01-01")).willReturn(apiResponse);
+		mockMvc.perform(get("/api/sales?date=2020-01-01"))
+			.andExpect(status().isOk())
+			.andExpect(content().string(containsString("\"error\":false")));
 	}
 }
